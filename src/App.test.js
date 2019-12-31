93375e0342d17a4d3090c80f0b84e8505337849d
MiaLearnsToCode <meroimia@gmail.com>
2019-12-31 12:31:02 +0000

    set up done

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Toggle Component", () => {
+  // check that the toggle container shows up in the page
+  it("Toggle compiles", () => {
+    const { getByTestId } = render(<App />)
+    const welcomeTitle = getByTestId("welcomeTitle")
+    expect(welcomeTitle).toBeInTheDocument()
+  })
+})
