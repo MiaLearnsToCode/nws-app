40cd36191f3ffa46e3e14c28d8f0544bec63d506
MiaLearnsToCode <meroimia@gmail.com>
2020-01-06 11:25:45 +0000

    state works in react-select

--- src/components/Form/FormSelect.js
+++ src/components/Form/FormSelect.js
@@ -4,12 +4,15 @@ import makeAnimated from 'react-select/animated';
 
 const animatedComponents = makeAnimated();
 
-const FormSelect = ({selectOptions}) => {
+const FormSelect = ({selectOptions, onChange}) => {
+
+  
   return (
     <Select
       closeMenuOnSelect={false}
       components={animatedComponents}
       isMulti
+      onChange={onChange}
       options={selectOptions}
       styles={{ multiValueLabel: styles => ({ ...styles, backgroundColor: '#00FFB2' }), multiValueRemove: styles => ({ ...styles, backgroundColor: '#00FFB2' }),}}
     />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import FormSelect from '../../components/Form/FormSelect'
 import {languages, countries, topics} from '../../components/Form/FormData'
 
 const Home = () => {
+  const [languagesChosen, handleChangeLanguages] = useState([]);
+  const [countriesChosen, handleChangeCountries] = useState([]);
+  const [topicsChosen, handleChangeTopics] = useState([]);
+  console.log(languagesChosen, topicsChosen, countriesChosen);
+
   return (
     <div data-testid='homeContainer'>
       <header data-testid='headerContainer'>
@@ -11,11 +16,11 @@ const Home = () => {
       </header>
       <form>
         <label>Languages</label>
-        <FormSelect selectOptions={[...languages]}/>
+        <FormSelect selectOptions={[...languages]} onChange={(e) => handleChangeLanguages(e.map(option => option.value))}/>
         <label>Countries</label>
-        <FormSelect selectOptions={[...countries]}/>
+        <FormSelect selectOptions={[...countries]} onChange={(e) => handleChangeCountries(e.map(option => option.value))}/>
         <label>Topics</label>
-        <FormSelect selectOptions={[...topics]}/>
+        <FormSelect selectOptions={[...topics]} onChange={(e) => handleChangeTopics(e.map(option => option.value))}/>
         <button data-testid='formButton'>Let&apos;s Go</button>
       </form>
       
