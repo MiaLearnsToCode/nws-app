23e7772331e066ff907501e7ae7124e35dd45b0b
MiaLearnsToCode <meroimia@gmail.com>
2020-01-10 16:03:02 +0000

    local storage check

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import "./stylesheets/main.scss";
 import Home from './pages/Home/Home';
 import News from './pages/News/News';
 
+const localStorageCheck = localStorage.getItem("languagesChosen") ? true : false
+
 const routes = {
-  "/": () => <Home />,
+  "/": () => localStorageCheck ? <News /> : <Home />,
   "/news": () => <News />
 };
 
 const App = () => {
   const routeResult = useRoutes(routes);
   return (
-    <main data-testid='appContainer'>
-      {routeResult}
-    </main>
+    <>
+      <main data-testid='appContainer'>
+        {routeResult}
+      </main>
+      <footer>
+        <p>
+          This is app was built with <span aria-label="love" role="img">♥️</span> by <a href="https://github.com/MiaLearnsToCode" rel="noopener noreferrer" target="_blank">Mia</a> and powered by <a href="https://newsapi.org/" rel="noopener noreferrer" target="_blank">News API</a>.
+        </p>
+      </footer>
+    </>
   );
 };
 
