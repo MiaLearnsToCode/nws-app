f6eb777fd4a2518c8828156fb85fe12914dcc559
MiaLearnsToCode <meroimia@gmail.com>
2020-01-14 17:55:57 +0000

    memoize comments

--- src/components/Http/Http.js
+++ src/components/Http/Http.js
@@ -7,7 +7,22 @@ const Http = ({ language, country, category, chosenCategory, handleClick }) => {
   const [articles, setData] = useState([]);
 
   useEffect(() => {
-    const getArticlesData = async (source) => {   
+
+    //if a function is called twice with the same arguments, it will return the same cached value the second time.
+    const memoize = (method) => {
+      //I'm caching the promise returned by the method, not the final value. 
+      //SO I don’t need to wait for the async method to return anything, before we cache it’s future value.
+      return async function () {
+        let cache = {};
+        let args = JSON.stringify(arguments);
+        cache[args] = cache[args] || method.apply(this, arguments);
+        return cache[args];
+      };
+    }
+
+    // 2. Get the top headlines from the sources returned from getSourcesData
+    const getArticlesData = memoize(async (source) => {
+      console.log('articles');
       try {
         const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source.id}&apiKey=${token}`);
         let articles = await res.json();
@@ -17,12 +32,14 @@ const Http = ({ language, country, category, chosenCategory, handleClick }) => {
           return article;
         });
         if (articles.length) setData(articles);
-      } catch(error) {
+      } catch (error) {
         console.log(error);
       };
-    };
+    });
 
-    const getSourcesData = async () => {
+    // 1. first I need to get the sources that may include articles with the user's choices
+    const getSourcesData = memoize(async () => {
+      console.log(category);
       try {
         const res = await fetch(`https://newsapi.org/v2/sources?language=${language}&country=${country}&category=${category}&apiKey=${token}`)
         let sources = await res.json()
@@ -30,23 +47,12 @@ const Http = ({ language, country, category, chosenCategory, handleClick }) => {
       } catch (error) {
         console.log(error)
       };
-    };
+    });
 
+    const memoizedData = memoize(getSourcesData);
     
-
-    const memoize = (method) => {
-      return async function () {
-        let cache = {};
-        let args = JSON.stringify(arguments);
-        cache[args] = cache[args] || method.apply(this, arguments);
-        console.log(cache[args]);
-        return cache[args];
-      };
-    }
-
-    const data = memoize(getSourcesData);
-    
-    data();
+    //After the first call, I’ll always get a cached result, even if I call one of these methods again before it has returned for the first time.
+    memoizedData();
 
   }, [category, country, language, token]);
 
